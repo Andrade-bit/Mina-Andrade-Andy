f6835fe48d14387b90c596850e90cc5a0bae556b
Joen-Reloba <j.reloba.559602@umindanao.edu.ph>
2026-09-23 21:50:48 +0800

    first contribution

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.login');
 });
 
-Route::get('/admin/login', function () {
-    return view('admin.login');
-})->name('admin.login');
+// Route::get('/admin/login', function () {
+//     return view('admin.login');
+// })->name('admin.login');
 
 Route::get('/admin/users', function () {
     return view('admin.user-management');
